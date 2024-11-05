338438cf23ab0036136bdc4783daa7ae5a16d315
Sergiot25 <slta_j@hotmail.com>
2024-11-05 11:03:45 +0100

    cambios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,28 +3,27 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className='bg-white p-0 text-center text-black shadow-md border-t-4 w-full'>
-      <div className='flex flex-col items-center'>
-        <p className='mb-4'>&copy; 2024 Aquí Cosmética Esmera</p>
+    <footer className='bg-fuchsia-200 p-6 text-center text-black shadow-md border-t-4' >
+      <div className='mb-4'>
+        <p>&copy; 2024 Aquí Cosmética Esmera</p>
+      </div>
+      <div className='flex justify-center space-x-8 mb-4'>
+        <Link to="https://www.facebook.com" target="_blank" className='text-black hover:text-slate-500'>
+          <FaFacebook className="w-8 h-8" />
+        </Link>
+        
+        <Link to="https://www.instagram.com" target="_blank" className='text-black hover:text-slate-500'>
+          <FaInstagram className="w-8 h-8" />
+        </Link>
+      </div>
 
-        <div className='flex space-x-8 mb-4'>
-          <Link to="https://www.facebook.com" target="_blank" className='text-black hover:text-slate-500'>
-            <FaFacebook className="w-8 h-8" />
-          </Link>
-          
-          <Link to="https://www.instagram.com" target="_blank" className='text-black hover:text-slate-500'>
-            <FaInstagram className="w-8 h-8" />
-          </Link>
-        </div>
-
-        <div className='flex space-x-8'>
-          <Link to="/politicas-de-privacidad" className='text-black hover:text-slate-500'>
-            Políticas de privacidad
-          </Link>
-          <Link to="/terminos-y-condiciones" className='text-black hover:text-slate-500'>
-            Términos y condiciones
-          </Link>
-        </div>
+      <div className='flex justify-center space-x-8'>
+        <Link to="/politicas-de-privacidad" className='text-black hover:text-slate-500'>
+          Políticas de privacidad
+        </Link>
+        <Link to="/terminos-y-condiciones" className='text-black hover:text-slate-500'>
+          Términos y condiciones
+        </Link>
       </div>
     </footer>
   );
